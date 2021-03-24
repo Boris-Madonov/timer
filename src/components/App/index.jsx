6a4a0698ca68cb29bb375ca93065740120e1136f
Boris Madonov <maboig@yandex.ru>
2021-03-24 12:44:53 +0300

    feat: создать структуру приложения

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
 import './index.css';
 
-function App() {
+export default function App() {
   return (
-    <div
-      className="App"
-    >
-      1
+    <div className="timer__page">
+      <h1 className="timer__title">
+        text
+      </h1>
+      <div className="timer__numbers">
+        <span className="timer__number">
+          00
+        </span>
+        <span className="timer__number">
+          :
+        </span>
+        <span className="timer__number">
+          00
+        </span>
+      </div>
+      <div className="timer__buttons">
+        <button
+          className="timer__button"
+          type="button"
+        >
+          Start
+        </button>
+        <button
+          className="timer__button"
+          type="button"
+        >
+          Stop
+        </button>
+        <button
+          className="timer__button"
+          type="button"
+        >
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
