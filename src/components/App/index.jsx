ebd3d97ea9d20bf80bfdf9d0a211022a936e3bcf
Boris Madonov <maboig@yandex.ru>
2021-03-24 21:22:21 +0300

    feat: добавить сброс таймера

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,26 +3,37 @@ import './index.css';
 
 export default function App() {
   const [title, setTitle] = useState('Начните отсчет!');
-  const [count, setCount] = useState(10);
+  const [count, setCount] = useState(25 * 60);
+  const [buttonStartDisabled, setButtonStartDisabled] = useState(false);
+  const [buttonStopDisabled, setButtonStopDisabled] = useState(true);
 
   const minutes = Math.floor(count / 60).toString().padStart(2, 0);
   const seconds = (count - minutes * 60).toString().padStart(2, 0);
   const interval = useRef(null);
 
+  const handlerButtonReset = () => {
+    clearInterval(interval.current);
+    setCount(25 * 60);
+    setTitle('Начните отсчет!');
+  };
+
   const handlerButtonStart = () => {
-    setTitle('Поехали!');
+    setButtonStartDisabled(true);
+    setButtonStopDisabled(false);
     interval.current = setInterval(() => {
       setCount((c = count) => {
         if (c >= 1) return c - 1;
-        setTitle('Отсчет закончен!');
-        return 0;
+        return handlerButtonReset();
       });
     }, 1000);
+    setTitle('Поехали!');
   };
 
   const handlerButtonStop = () => {
-    setTitle('Начните отсчет!');
+    setButtonStartDisabled(false);
+    setButtonStopDisabled(true);
     clearInterval(interval.current);
+    setTitle('Не останавливайтесь!');
   };
 
   return (
@@ -46,6 +57,7 @@ export default function App() {
           className="timer__button"
           type="button"
           onClick={handlerButtonStart}
+          disabled={buttonStartDisabled}
         >
           Start
         </button>
@@ -53,12 +65,14 @@ export default function App() {
           className="timer__button"
           type="button"
           onClick={handlerButtonStop}
+          disabled={buttonStopDisabled}
         >
           Stop
         </button>
         <button
           className="timer__button"
           type="button"
+          onClick={handlerButtonReset}
         >
           Reset
         </button>
