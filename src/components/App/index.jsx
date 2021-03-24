a01417632d8ad1a024d6b3f208b5208121de7b76
Boris Madonov <maboig@yandex.ru>
2021-03-24 20:28:03 +0300

    feat: добавить запуск таймера

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 export default function App() {
+  const [title, setTitle] = useState('Начните отсчет!');
+  const [count, setCount] = useState(25 * 60);
+
+  const minutes = Math.floor(count / 60).toString().padStart(2, 0);
+  const seconds = (count - minutes * 60).toString().padStart(2, 0);
+
+  const handlerButtonStart = () => {
+    setTitle('Поехали!');
+    setInterval(() => {
+      setCount((c = count) => c - 1);
+    }, 1000);
+  };
+
   return (
     <div className="timer__page">
       <h1 className="timer__title">
-        text
+        {title}
       </h1>
       <div className="timer__numbers">
         <span className="timer__number">
-          00
+          {minutes}
         </span>
-        <span className="timer__number">
+        <span className="timer__colon">
           :
         </span>
         <span className="timer__number">
-          00
+          {seconds}
         </span>
       </div>
       <div className="timer__buttons">
         <button
           className="timer__button"
           type="button"
+          onClick={handlerButtonStart}
         >
           Start
         </button>
